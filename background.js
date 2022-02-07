d15ca45ebc8c6b1d61433a23e7dd4c7085490fe0
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-02-07 20:33:36 +0000

    Add jquery

--- background.js
+++ background.js
@@ -1,9 +1,7 @@
 const authors = [];
 
 for (const author of authors) {
-  jQuery(`a.author[href="https://bikepost.ru/profile/${author}/"]`)
-    .closest('.comment-wrapper')
+  $(`a.author[href="https://bikepost.ru/profile/${author}/"]`)
+    .closest(".comment-wrapper")
     .css("background-color", "red");
 }
-
-
