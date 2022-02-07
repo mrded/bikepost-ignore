0efb707cf3281b78c1e0fe2aa4b2d0c5e838e82e
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-02-07 21:35:39 +0000

    Hide ignored comments

--- content.js
+++ content.js
@@ -8,6 +8,7 @@ const hideComments = (authors = []) => {
   for (const author of authors) {
     $(`a.author[href="https://bikepost.ru/profile/${author}/"]`)
       .closest(".comment-wrapper")
-      .css("background-color", "red");
+      .css("display", "none");
+    // .css("background-color", "red");
   }
 };
