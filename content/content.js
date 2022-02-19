2bfc95ba4967eda684d9d8daa5eee42ea4f0b223
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-02-19 15:31:25 +0000

    feat: show an action link to ignore users.

--- content/content.js
+++ content/content.js
@@ -19,3 +19,52 @@ const hideComments = (authors = []) => {
     // .css("background-color", "red");
   }
 };
+
+const ignoreLinkHandler = el => {
+  const username = el.target.getAttribute("data");
+
+  browser.storage.local.get(["authors"], result => {
+    const authorsList = (result.authors || "").split(",").map(x => x.trim());
+
+    browser.storage.local.set(
+      {
+        authors: [...authorsList, username].join(", ")
+      },
+      () => {
+        alert(
+          `Пользователь ${username} добавлен в игнор-лист. Обновите страницу.`
+        );
+      }
+    );
+  });
+
+  return false;
+};
+
+const makeIgnoreLink = username => {
+  const ignoreLink = document.createElement("a");
+
+  ignoreLink.innerHTML = "Игнорировать";
+  ignoreLink.setAttribute("href", "javascript:;");
+  ignoreLink.setAttribute("class", "ignore");
+  ignoreLink.setAttribute("data", username);
+
+  return ignoreLink;
+};
+
+// Set Ignore links.
+const comments = $("div.comment-wrapper a.author");
+
+for (const comment of comments) {
+  const username = $(comment).text();
+
+  const ignoreLink = makeIgnoreLink(username);
+  const ignoreUl = document.createElement("ul").appendChild(ignoreLink);
+
+  comment.closest(".info ul").append(ignoreUl);
+}
+
+// Set Ignore action handler.
+[...document.querySelectorAll("div.comment-wrapper a.ignore")].forEach(el =>
+  el.addEventListener("click", ignoreLinkHandler)
+);
